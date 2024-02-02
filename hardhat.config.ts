d0973fc2ad87fd6937b481853513b3a5ba610b66
nguyenduc <nguyenduc0453@gmail.com>
2024-02-02 15:00:28 +0700

    add chain config

--- hardhat.config.ts
+++ hardhat.config.ts
@@ -3,6 +3,14 @@ import "@nomicfoundation/hardhat-toolbox";
 
 const config: HardhatUserConfig = {
   solidity: "0.8.19",
+  networks: {
+    base: {
+      url: "https://base-goerli.public.blastapi.io",
+      chainId: 84531,
+      // @ts-ignore
+      accounts: [process.env.PRIVATE_KEY]
+    },
+  }
 };
 
 export default config;
